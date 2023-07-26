eee90573ab78c8b02fb0dc661c99c14e3684e794
kowalski-prog <138756028+kowalski-prog@users.noreply.github.com>
2023-07-26 12:48:43 +0200

    Update konto.php
    usunięcie danych wrażliwych

--- konto.php
+++ konto.php
@@ -1,18 +1,18 @@
 <?php
 
-$host = 's149.cyber-folks.pl';
-$db_user ='komarkus_intencje';
+$host = 'localhost';
+$db_user ='user_intencje';
 $db_password = 'Intencje123**';
-$db_name = 'komarkus_intencje';
+$db_name = 'user_intencje';
 $db_conn = mysqli_connect($host,$db_user,$db_password) 
 or die ('Odpowiedź: Błąd połączenia z serwerem $host');
 mysqli_select_db($db_conn, $db_name) or die('Trwa konserwacja bazy danych… Odśwież stronę za kilka sekund.');
 
 
 
-    $name= mysqli_real_escape_string($db_conn, "Jakub");
-    $password = mysqli_real_escape_string($db_conn, "Banaszak");
-    $parafia = mysqli_real_escape_string($db_conn, "intencje");
+    $name= mysqli_real_escape_string($db_conn, "username");
+    $password = mysqli_real_escape_string($db_conn, "username");
+    $parafia = mysqli_real_escape_string($db_conn, "parafianame");
     $pass = password_hash($password, PASSWORD_DEFAULT);
     echo "INSERT INTO konta (name, pass) VALUES ('$name', '$pass')";
     if (mysqli_query($db_conn, "INSERT INTO konta (name, pass, parafia) VALUES ('$name', '$pass', '$parafia')"))
